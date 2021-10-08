9d5a44b8fca17fe678b7dcbc0a7f59e42bad0a35
Roman Medvedev <romankazan88@gmail.com>
2021-10-08 21:18:48 +0300

    Bonus miles

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 1000;
+        int miles = 20;
+
+
+        int bonus = price / miles;
+        System.out.println(bonus);
+
+    }
+}
